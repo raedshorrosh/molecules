1ec7314b7973513a9c8e645793e6714d15f929bc
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2026-07-02 14:31:29 +0300

    Delete test-pubchem-bond.js

--- test-pubchem-bond.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-fetch('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/5793/JSON')
-  .then(r => r.json())
-  .then(d => {
-      console.log(d);
-  })
-  .catch(console.error);
