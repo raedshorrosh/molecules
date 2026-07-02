2ca93f7a07484f2e540da6336923d9c5285e9406
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2026-07-02 14:29:11 +0300

    Delete fetch-al.js

--- fetch-al.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-fetch('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/L-Alanine/JSON?record_type=2d')
-  .then(r => r.json())
-  .then(d => {
-      fs.writeFileSync('l-alanine.json', JSON.stringify(d, null, 2));
-      console.log('Saved');
-  })
-  .catch(console.error);
